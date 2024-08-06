1da85f204e510b546c133d5ccebdce7722b25113
mithileshporate <mithileshporate2k25@gmail.com>
2024-08-07 00:40:29 +0530

    forth

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 export const dbConnection = () => {
   mongoose
     .connect(process.env.MONGO_URI, {
-      dbName: "NGO_PROJECT",
+      dbName: "NGO_Database",
     })
     .then(() => {
       console.log("Connected to database!");
